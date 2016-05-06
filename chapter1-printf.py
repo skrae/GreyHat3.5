1e1ed2654f60ad991704d00c47f9147aeaf89426
ThreeNoThree <justincbrenneman@gmail.com>
2016-05-05 21:07:54 -0600

    Added files via upload

---
+++ chapter1-printf.py
@@ -0,0 +1,9 @@
+##Seitz, J. (2008). Grey Hat Python: Python programming for hackers and reverse engineers. San Francisco, CA: No Starch Press. 
+##Chapter 1 page 8
+##Rewritten by Justin Brenneman
+
+from ctypes import *
+
+libc = CDLL("libc.so.6")
+message_string = b"Hello world!\n"
+libc.printf(b"Testing:  %s", message_string)
